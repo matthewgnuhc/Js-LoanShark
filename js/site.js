2ad5be840d97e86b3e61b2065d07b51424a45d2a
Matthew Chung <matthewgnuhc@gmail.com>
2021-10-08 18:05:55 -0700

    init commit

--- js/site.js
+++ js/site.js
@@ -1,20 +1,45 @@
+//Mortgage Calculator
+//Step 1 - Get User inputs - this will access the DOM and call all other functions
+function getValues() {
+    let loanAmount = document.getElementById("someId").value;
 
-// Displays a message to the user
-function displayMessage() {
-    // Declare our variable
-    let msg = "";
-    // Pull the value from the input box
-    msg = document.getElementById("message").value;
-    // Alert the user
-    alert(msg);
+    let term = document.getElementById("anotherId").value;
+
+    let interestRate = document.getElementById("thirdId").value;
+
+    let calculatedRate = calcRate(interestRate);
+
+    let payment = calcPayment(loanAmount, calculatedRate, term);
+
+    let totalPayments = calcPaymentSchedule(loanAmount, calculatedRate, term, payment);
+
+    displayData(totalPayments);
+
 }
 
-function displayMessage2() {
-    
-    let msg = "";
-    
-    msg = document.getElementById("message").value;
-    
-    Swal.fire(msg);
-}
+//Step 2 - Calculate the interest rate - this is the division by 1200 from the PDF
+function calcRate() {}
 
+//Step 3 - Calculate monthly payment - Amount times rate divided by the complicated math from the PDF Math.pow()
+function calcPayment() {}
+
+//Step 3.5 - Calculate the interest rate based on the current balance in rate from step 2
+//This function is called in Step 4, but you need to build it first
+//Calculates interest per payment
+function calcInterest() {}
+
+//Step 4 - Now that we know the rate and monthly payment we build out our payment schedule
+//This is the big function of the application - create your object in this function
+//let obj = {
+//     month:
+//     payment:
+//     principal:
+//     interest:
+//     totalInterest:
+//     balance:
+// }
+function calcPaymentSchedule() {}
+
+//Step 5 - Display all of the calculated information
+//Use the template structure from FizzBuzz to output your data
+function displayData() {}
